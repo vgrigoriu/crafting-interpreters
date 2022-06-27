43d475c2cb29ac7522c8a30524b45122691fa282
Victor Grigoriu <grigoriu@adobe.com>
2022-06-27 20:23:55 +0300

    delete .gradle, implement main

--- kotlin/klox/src/main/kotlin/Main.kt
+++ kotlin/klox/src/main/kotlin/Main.kt
@@ -1,7 +1,45 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.nio.charset.Charset
+import java.nio.file.Files
+import java.nio.file.Paths
+import kotlin.system.exitProcess
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    val errorReporter = ErrorReporter()
+    if (args.size > 1) {
+        println("Usage: klox [script]")
+        exitProcess(64)
+    } else if (args.size == 1) {
+        runFile(args[0], errorReporter)
+    } else {
+        runPrompt(errorReporter)
+    }
+}
+
+fun runFile(path: String, errorReporter: ErrorReporter) {
+    val bytes = Files.readAllBytes(Paths.get(path))
+    run(String(bytes, Charset.defaultCharset()), errorReporter)
+    if (errorReporter.hadError) {
+        exitProcess(65)
+    }
+}
+
+fun runPrompt(errorReporter: ErrorReporter) {
+    val input = InputStreamReader(System.`in`)
+    val reader = BufferedReader(input)
+
+    while (true) {
+        print("> ")
+        val line = reader.readLine() ?: break
+        run(line, errorReporter)
+        errorReporter.reset()
+    }
+}
+
+fun run(source: String, errorReporter: ErrorReporter) {
+    val scanner = Scanner(source, errorReporter)
+    val tokens = scanner.scanTokens()
+
+    tokens.forEach { print(it) }
 }
---
+++ kotlin/klox/src/main/kotlin/Scanner.kt
@@ -0,0 +1,5 @@
+class Scanner(source: String, errorReporter: ErrorReporter) {
+    fun scanTokens(): List<Token> {
+        TODO("Not yet implemented")
+    }
+}
