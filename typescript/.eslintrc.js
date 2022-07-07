dbdcf1e05ead78fd35ae9e2a7bc4b6ff0a802507
Victor Grigoriu <grigoriu@adobe.com>
2022-07-07 14:23:38 +0300

    start typescript project

---
+++ typescript/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true
+  },
+  extends: [
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+  }
+}
