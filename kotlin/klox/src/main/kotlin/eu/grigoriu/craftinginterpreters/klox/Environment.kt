33063d28fed78cd83219b8b156861377d9a1c113
Victor Grigoriu <grigoriu@adobe.com>
2022-07-13 08:11:27 +0300

    Add nested scopes (8.5.1)

--- kotlin/klox/src/main/kotlin/eu/grigoriu/craftinginterpreters/klox/Environment.kt
+++ kotlin/klox/src/main/kotlin/eu/grigoriu/craftinginterpreters/klox/Environment.kt
@@ -1,23 +1,34 @@
 package eu.grigoriu.craftinginterpreters.klox
 
-class Environment {
+class Environment(private val enclosing: Environment?) {
     private val values = mutableMapOf<String, Any?>()
+
+    constructor() : this(null)
 
     fun define(name: String, value: Any?) {
         values[name] = value
     }
 
     fun assign(name: Token, value: Any?) {
-        if (!values.containsKey(name.lexeme)) {
-            throw RuntimeError(name, "Undefined variable '${name.lexeme}'.")
+        if (values.containsKey(name.lexeme)) {
+            values[name.lexeme] = value
+            return
         }
-        values[name.lexeme] = value
+
+        if (enclosing != null) {
+            enclosing.assign(name, value)
+        }
+
+        throw RuntimeError(name, "Undefined variable '${name.lexeme}'.")
     }
 
     fun get(name: Token): Any? {
-        if (!values.containsKey(name.lexeme)) {
-            throw RuntimeError(name, "Undefined variable '${name.lexeme}'.")
+        if (values.containsKey(name.lexeme)) {
+            return values[name.lexeme]
         }
-        return values[name.lexeme]
+        if (enclosing != null) {
+            return enclosing.get(name)
+        }
+        throw RuntimeError(name, "Undefined variable '${name.lexeme}'.")
     }
 }
