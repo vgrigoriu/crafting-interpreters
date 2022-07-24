3775b5f9f4d52d00fe5fb8241ce8fa8fa063a456
Victor Grigoriu <grigoriu@adobe.com>
2022-07-24 17:33:37 +0300

    Resolve variable declarations (11.3.2)

--- kotlin/klox/src/main/kotlin/eu/grigoriu/craftinginterpreters/klox/Resolver.kt
+++ kotlin/klox/src/main/kotlin/eu/grigoriu/craftinginterpreters/klox/Resolver.kt
@@ -3,12 +3,20 @@ package eu.grigoriu.craftinginterpreters.klox
 import java.util.Stack
 
 abstract class Resolver(private val interpreter: Interpreter) : Expr.Visitor<Unit>, Stmt.Visitor<Unit> {
-    private val scopes = Stack<Map<String, Boolean>>()
+    private val scopes = Stack<MutableMap<String, Boolean>>()
 
     override fun visitBlockStmt(stmt: Stmt.Block) {
         beginScope()
         resolve(stmt.statements)
         endScope()
+    }
+
+    override fun visitVarStmt(stmt: Stmt.Var) {
+        declare(stmt.name)
+        if (stmt.initializer != null) {
+            resolve(stmt.initializer)
+        }
+        define(stmt.name)
     }
 
     private fun beginScope() {
@@ -31,5 +39,21 @@ abstract class Resolver(private val interpreter: Interpreter) : Expr.Visitor<Uni
 
     private fun resolve(expr: Expr) {
         expr.accept(this)
+    }
+
+    private fun declare(name: Token) {
+        if (scopes.empty()) {
+            return
+        }
+        // Variable not ready yet.
+        scopes.peek()[name.lexeme] = false
+    }
+
+    private fun define(name: Token) {
+        if (scopes.empty()) {
+            return
+        }
+        // Variable ready.
+        scopes.peek()[name.lexeme] = true
     }
 }
